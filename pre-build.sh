b84f1549cdf2431f157216830591e3546ddbe230
marcel0ll <marcelo@lotuz.dev>
2022-02-28 23:38:05 -0300

    fix building

--- pre-build.sh
+++ pre-build.sh
@@ -13,13 +13,13 @@ replace() {
   sed -e "s/{{$1}}/$2/" $FILE
 }
 
-jq -r '.lexicon | to_entries[] | "\(.key) \(.value)"' $DIALECT \
+jq -r '.keywords + .literals + .special | to_entries[] | "\(.key) \(.value)"' $DIALECT \
   | xargs -n 2 sh -c $'sed -i -e "s/{{$0}}/$1/g" '$FILE
 
 jq -r '.dialect' $DIALECT \
   | xargs -n 1 sh -c $'sed -i -e "s/{{dialect}}/$0/g" '$FILE
 
-jq -r '.lexicon | to_entries[] | "\(.key) \(.value)"' $DIALECT \
+jq -r '.keywords + .literals + .special  | to_entries[] | "\(.key) \(.value)"' $DIALECT \
   | xargs -n 2 sh -c $'sed -i -e "s/{{$0}}/$1/g" '$HIGHLIGHT
 
 # Remove tree-sitter bindings
@@ -35,6 +35,7 @@ rm \
 # Run tree-sitter generate
 tree-sitter generate
 
+echo adding to scanner
 sed '12 i         "src/scanner.c",' ./binding.gyp
 
 node-gyp configure
